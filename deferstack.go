5c897dfdbc2f25d27392ef95c15339bb2cfb0bd4
HystericalDragon <HystericalDragons@proton.me>
2023-12-23 09:32:50 +0800

    fix: change lenth to fix error lock
    Signed-off-by: HystericalDragon <HystericalDragons@proton.me>

--- deferstack.go
+++ deferstack.go
@@ -33,13 +33,18 @@ func (d *deferstack) Add(a ...func()) {
 func (d *deferstack) Length() int {
 	d.mu.Lock()
 	defer d.mu.Unlock()
+	return d.length()
+}
+
+// Use it in internal. It doesn't has lock!
+func (d *deferstack) length() int {
 	return len(d.funcs)
 }
 
 func (d *deferstack) Run() {
 	d.mu.Lock()
 	defer d.mu.Unlock()
-	for i := d.Length() - 1; i >= 0; i-- {
+	for i := d.length() - 1; i >= 0; i-- {
 		if d.funcs[i] == nil {
 			continue
 		}
@@ -59,8 +64,8 @@ func (d *deferstack) Remove(num int) error {
 	if num < 0 {
 		return errors.New("num less than zero")
 	}
-	if num > d.Length() {
-		return fmt.Errorf("the length is %d, but got %d", d.Length(), num)
+	if num > d.length() {
+		return fmt.Errorf("the length is %d, but got %d", d.length(), num)
 	}
 	d.mu.Lock()
 	defer d.mu.Unlock()
